498da5d57cd5bf2c8011cfaf0a01b7389c640d1e
adriancancode <vazqueza2004@gmail.com>
2025-05-08 14:05:27 -0700

    Fix up App.jsx

--- memory-game/src/App.jsx
+++ memory-game/src/App.jsx
@@ -1,29 +1,44 @@
 import { useState, useEffect } from 'react'
 import './styles/App.css'
-import './components/Card'
+import Card from './components/Card';
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [loading, isLoading] = useState(true);
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+
   const api_key = import.meta.env.CAT_API_ACCESS_KEY;
-  const url = ` https://api.thecatapi.com/v1/images/search?limit=9?api_key=${api_key}`;
+  const url = ` https://api.thecatapi.com/v1/images/search?limit=10?api_key=${api_key}`;
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch(url);
+        console.log("response status: ", response.status);
         const data = await response.json();
         console.log(data);
         setData(data);
       } catch (error) {
         console.error('Error fetching data:', error);
+        setError(error);
+      } finally {
+        setLoading(false);
       }
     };
+
     fetchData();
   }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>Cat Memory Game</h1>
+      <p>Score: {score}</p>
+      <p>Best Score: {bestScore}</p>
+
       <div className='card-container'>
         {data.map((cat) => (
           <Card title={cat.breeds.name} imageUrl={cat.url}/>
