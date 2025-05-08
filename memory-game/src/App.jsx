f8be4c7377030bcd525053a8359ee5cff6fcd20e
adriancancode <vazqueza2004@gmail.com>
2025-05-08 15:30:37 -0700

    Finalized game functionality and styling

--- memory-game/src/App.jsx
+++ memory-game/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   const [clickedCats, setClickedCats] = useState([]);
 
   const api_key = import.meta.env.VITE_CAT_API_ACCESS_KEY;
-  const url = ` https://api.thecatapi.com/v1/images/search?limit=10?${api_key ? `&api_key=${api_key}` : ''}`;
+  const url = ` https://api.thecatapi.com/v1/images/search?limit=14&has_breeds=1${api_key ? `&api_key=${api_key}` : ''}`;
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -35,12 +35,41 @@ function App() {
   }, [url]);
 
   if (loading) {
-    return <div className='loading'>Loading...</div>;
+    return (
+      <div className='app'>
+        <h1>Cat Memory Game</h1>
+        <p>Score: {score}</p>
+        <p>Best Score: {bestScore}</p>
+        <p>Click on a cat to earn points, but don't click on the same cat twice!</p>
+        <h2>Loading cats...</h2>
+      </div>
+      
+    );
   }
 
   if (error) {
     return <div className='error'>Error: {error.message}</div>;
   }
+
+  const shuffleCats = () => {
+    const shuffledCats = [...cats].sort(() => Math.random() - 0.5);
+    setCats(shuffledCats);
+  }
+  const handleCardClick = (catId) => {
+    if (clickedCats.includes(catId)) {
+      alert("You already clicked this cat!");
+      setScore(0);
+      setClickedCats([]);
+    } else {
+      setClickedCats([...clickedCats, catId]);
+      setScore(score + 1);
+      if (score + 1 > bestScore) {
+        setBestScore(score + 1);
+      }
+    }
+    shuffleCats();
+  }
+
   return (
     <div className='app'>
       <h1>Cat Memory Game</h1>
